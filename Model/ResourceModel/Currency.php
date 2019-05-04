ac5f66a5ae7e8f0d0cfbe2a03fd27f91ffa825c5
Yannis Livasov <ilib@mail.ru>
2019-05-04 22:46:11 +0300

    fix display currency symbol

--- Model/ResourceModel/Currency.php
+++ Model/ResourceModel/Currency.php
@@ -11,6 +11,13 @@ class Currency extends \Magento\Directory\Model\ResourceModel\Currency
 {
 
     /**
+     * Currency params
+     *
+     * @var array
+     */
+    private $_currencyData;
+    
+    /**
      * Substitute core table directory_currency_rate
      *
      * @return void
@@ -89,29 +96,72 @@ class Currency extends \Magento\Directory\Model\ResourceModel\Currency
     /**
      * Return currency names
      *
-     * @param string|array $currency
+     * @param string|array $code
      * @return array
      * @SuppressWarnings(PHPMD.Ecg.Sql.SlowQuery)
      */
     public function getCurrencyNames($code)
     {
-        $connection = $this->getConnection();
-        $select = $connection->select()->from(
-            $this->getTable('kozeta_currency_coin'),
-            ['code','name']
-        )->where(
-            'code IN(?)',
-            $code
-        );
-        // @codingStandardsIgnoreStart
-        $rowSet = $connection->fetchAll($select);
-        // @codingStandardsIgnoreEnd
-        $result = [];
-        
-        foreach ($rowSet as $row) {
-            $result[$row['code']] = $row['name'];
+        return $this->getCurrencyParamByCode($code, 'name');
+    }
+    
+    /**
+     * Return currency symbol
+     *
+     * @param string|array $code
+     * @return array
+     * @SuppressWarnings(PHPMD.Ecg.Sql.SlowQuery)
+     */
+    public function getCurrencySymbol($code) 
+    {
+        return $this->getCurrencyParamByCode($code, 'symbol');
+    }
+    
+    /**
+     * Return currency parameters
+     *
+     * @param string|array $code
+     * @param string|array $param
+     * @return array
+     * @SuppressWarnings(PHPMD.Ecg.Sql.SlowQuery)
+     */
+    private function getCurrencyParamByCode($code, $param = null)
+    {
+        if (!is_array($code)) {
+            $code = [$code];
+        }
+        foreach ($code as $k => $c) {
+            if (isset($this->_currencyData[$c])) {
+                unset($code[$k]);
+                continue;
+            }
         }
 
+        if (isset($this->_currencyData)) {
+            foreach ($this->_currencyData as $row) {
+                $result[$row['code']] = $param ? $row[$param] : $row;
+            }
+        }
+
+        if ($code){
+            $connection = $this->getConnection();
+            $select = $connection->select()->from(
+                $this->getTable('kozeta_currency_coin'),
+                ['*']
+            )->where(
+                'code IN(?)',
+                $code
+            );
+
+            // @codingStandardsIgnoreStart
+            $rowSet = $connection->fetchAll($select);
+            // @codingStandardsIgnoreEnd
+
+            foreach ($rowSet as $row) {
+                $result[$row['code']] = $param ? $row[$param] : $row;
+                $this->_currencyData[$row['code']] = $row;
+            }
+        }
         return $result;
     }
 }
